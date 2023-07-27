e55135549660c3e0811389e45f1e35156a734387
Mthobisi-za <mthobisimtho333@gmail.com>
2023-07-27 11:20:24 +0200

    done

---
+++ parallex.js
@@ -0,0 +1,8 @@
+// var images = document.querySelector('.top_small');
+// new simpleParallax(images);
+// new simpleParallax(images, {
+//     overflow: true,
+//     delay: .6,
+//     transition: 'cubic-bezier(0,0,0,1)',
+//     scale: 1.5
+// });
